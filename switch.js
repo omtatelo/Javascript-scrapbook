73b51eaae3c1c6925475b983399826efb3d817b0
Shelley Valdés <35204421+omtatelo@users.noreply.github.com>
2020-08-19 02:23:11 -0500

    switch just reviewing

---
+++ switch.js
@@ -0,0 +1,10 @@
+var signo = prompt("¿Qué signo eres?")
+console.log(signo)
+
+switch (signo){
+    case "Capricorrnio":
+        console.log("El más chingón")
+        break
+    default:
+        console.log("me dio hueva hacer los demás lol")
+}
