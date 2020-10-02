ecdd891976e047996520aad3a74aee0f603bc90e
Shelley Valdés <35204421+omtatelo@users.noreply.github.com>
2020-10-02 18:04:48 -0500

    clases

--- clases.js
+++ clases.js
@@ -11,16 +11,25 @@ Persona.prototype.saludar = function(){
     console.log(`Hola, me llamo ${this.nombre} ${this.apellido}.`)
 }
 
-Persona.prototype.esAlto = function(estatura){
-    if(estatura >= 1.8)
+Persona.prototype.esAlto = function(){
+    console.log("validando estatura")
+    if(this.estatura >= 1.80){
+        console.log(`Soy ${this.nombre} y soy alto.`)
+    }else{
+        console.log(`Soy ${this.nombre} y no soy tan alto.`)
+    }
 }
 
-var sacha = new Persona("Sacha", "Lifszyc")
+var erix = new Persona("Erix", "Rivas", 1.75 )
 //new crea un nuevo objeto con el prototipo de persona
 
-var juan = new Persona("Juan", "Agudelo")
-var jose = new Persona("José", "Flores")
+var juan = new Persona("Juan", "Agudelo", 1.80 )
+var jose = new Persona("José", "Flores", 1.73 )
 
-sacha.saludar()
-juan.saludar()
-jose.saludar()
+erix.esAlto()
+juan.esAlto()
+jose.esAlto()
+
+//sacha.saludar()
+//juan.saludar()
+//jose.saludar()
