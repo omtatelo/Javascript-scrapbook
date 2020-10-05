58572e0be8ff26a1c3d429ea10dab2a2240ed66a
Shelley Valdés <35204421+omtatelo@users.noreply.github.com>
2020-10-05 03:27:23 -0500

    herencia prototipal

--- clases.js
+++ clases.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo, prototipoPadre){
+    var fn = function(){}
+    fn.prototype = prototipoPadre.prototype
+    prototipoHijo.prototype = new fn
+    prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, estatura){
     //console.log("Me ejecutaron")
     this.nombre = nombre
@@ -12,7 +19,7 @@ Persona.prototype.saludar = function(){
 }
 
 Persona.prototype.esAlto = function(){
-    console.log("validando estatura")
+    //console.log("validando estatura")
     if(this.estatura >= 1.80){
         console.log(`Soy ${this.nombre} y soy alto.`)
     }else{
@@ -20,16 +27,27 @@ Persona.prototype.esAlto = function(){
     }
 }
 
-var erix = new Persona("Erix", "Rivas", 1.75 )
-//new crea un nuevo objeto con el prototipo de persona
+function Desarrollador(nombre, apellido){
+     this.nombre = nombre
+     this.apellido = apellido
+}
 
-var juan = new Persona("Juan", "Agudelo", 1.80 )
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function(){
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`)
+}
+
+
+//new crea un nuevo objeto con el prototipo de persona
+var erix = new Persona("Erix", "Rivas", 1.75 )
+var juan = new Desarrollador("Juan", "Agudelo", 1.80 )
 var jose = new Persona("José", "Flores", 1.73 )
 
 erix.esAlto()
 juan.esAlto()
 jose.esAlto()
 
-//sacha.saludar()
-//juan.saludar()
-//jose.saludar()
+erix.saludar()
+juan.saludar()
+jose.saludar()
