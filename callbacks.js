8109dfbdefbb141834384e948fd651bd41642cbe
Shelley Valdés <creative@shelvaldes.com>
2020-11-29 01:05:27 -0600

    callback hell

--- callbacks.js
+++ callbacks.js
@@ -1,22 +1,36 @@
+//Para usarse cuando el js se ejecute en un html
+//var script = document.createElement('script');
+//script.src = '//code.jquery.com/jquery-3.5.1.min.js';
+//script.type = 'text/javascript';
+//document.getElementsByTagName('head')[0].appendChild(script);
+
+//hacer un pedo con node y luego poner esto
+//import * as jquery from 'code.jquery.com/jquery-3.5.1.min.js';
+
 const API_URL = 'https://swapi.dev/api/'
 const PEOLE_URL = 'people/:id'
 
+const opts = { crossDomain: true }
 
 
-const opts = {crossDomain: true}
-
-const onPeopleResponse = function(persona){
-    console.log(`Hola, yo soy ${persona.name}`)
-}
-
-function obtenerPersonaje(id){
+function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOLE_URL.replace(`:id`, id)}`
     //Callback,  función que se ejecutará n el futuro.
-    $.get(url, opts, onPeopleResponse)
+    $.get(url, opts, 
+    })
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(5)
+
+//Hacer los callbacks en orden
+obtenerPersonaje(1, function(){
+    obtenerPersonaje(5, function(){
+        obtenerPersonaje(10, function(){
+            obtenerPersonaje(14)
+        })
+    })
+})
+
+
 
 
 
